8750d608d6745b392af54317d4ba53897690b5f9
brianbickes <brianbickes@gmail.com>
2020-10-21 11:43:54 -0700

    added some conditionals for practice, will be changing these for project but this will show work for that.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 class App extends Component {
   render(){ 
+    const name = 'John Doe';
+    const loading = false; 
+    const showName = false; 
      return (
-    <div className="App">
-     <h1>Hello From React</h1>
+  <div className="App">
+    {loading ? <h4>Loading ... </h4> : <h1>Hello {showName && name}</h1>}
     </div>
   );
   }
