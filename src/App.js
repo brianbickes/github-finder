9544e0176153f738ca63c7f169ed4e115b3f4269
brianbickes <brianbickes@gmail.com>
2020-10-21 11:46:17 -0700

    Adding Navbar. Added components and layout to src.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 
 class App extends Component {
   render(){ 
-    const name = 'John Doe';
-    const loading = false; 
-    const showName = false; 
+    
      return (
   <div className="App">
-    {loading ? <h4>Loading ... </h4> : <h1>Hello {showName && name}</h1>}
+    <h1>Hello World</h1> 
     </div>
   );
   }
