b8db44642857cf16273ba708bb11557bd1de6a4d
brianbickes <brianbickes@gmail.com>
2020-10-26 15:13:10 -0700

    Code clean-up

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-
-  render() {
-const {login, avatar_url, html_url} = this.props.user; 
-
+const UserItem = ({ user: {login, avatar_url, html_url} }) => {
     return (
       <div className='card text-center'>
         <img
@@ -21,7 +18,10 @@ const {login, avatar_url, html_url} = this.props.user;
         </div>
       </div>
     );
-  }
-}
+  };
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
