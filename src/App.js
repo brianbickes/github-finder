f3827ff3fd70c1bed2b697ab27f2cda357e4f4b9
brianbickes <brianbickes@gmail.com>
2020-10-26 13:59:50 -0700

    changing state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users'
 
 class App extends Component {
   render(){ 
@@ -9,7 +9,10 @@ class App extends Component {
      return (
   <div className="App">
     <Navbar  />
-    <UserItem />
+    <div className= "container">
+      <Users />
+    </div>
+    
     </div>
   );
   }
