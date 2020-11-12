42f8962c9c0675e0fa5569a86f063d5c0242045b
brianbickes <brianbickes@gmail.com>
2020-11-12 10:24:31 -0700

    Installed axios, imported axios to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users'
+import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component {
+componentDidMount() {
+  
+}
+
   render(){ 
     
      return (
