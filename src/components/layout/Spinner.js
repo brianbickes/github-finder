3945a000710b699f293a636372de6c7ceaaa07f6
brianbickes <brianbickes@gmail.com>
2020-11-12 15:56:33 -0700

    Added state for user and loading, 30 users now present on page.

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,14 +1,12 @@
 import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
 
-export const spinner = () => (
-  <Fragment>
+export const Spinner = () => <Fragment>
     <img
       src={spinner}
       alt='Loading...'
       style={{ width: '200px', margin: 'auto', display: 'block' }}
     />
   </Fragment>
-);
 
 export default Spinner;
